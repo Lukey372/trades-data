98478fe3ca59d36f50c64e6113e81be5bbac20e9
Lukey372 <100325058+Lukey372@users.noreply.github.com>
2025-03-12 17:05:16 +1100

    Create walletConfig.ts

---
+++ src/walletConfig.ts
@@ -0,0 +1,37 @@
+export interface WalletInfo {
+  name: string;
+}
+
+export const walletConfig: { [walletAddress: string]: WalletInfo } = {
+  "JDd3hy3gQn2V982mi1zqhNqUw1GfV2UL6g76STojCJPN": { name: "West" },
+  "GwoFJFjUTUSWq2EwTz4P2Sznoq9XYLrf8t4q5kbTgZ1R": { name: "Levis" },
+  "EHg5YkU2SZBTvuT87rUsvxArGp3HLeye1fXaSDfuMyaf": { name: "TIL" },
+  "BTf4A2exGK9BCVDNzy65b9dUzXgMqB4weVkvTMFQsadd": { name: "Kev" },
+  "2CXbN6nuTTb4vCrtYM89SfQHMMKGPAW4mvFe6Ht4Yo6z": { name: "MoneyMaykah" },
+  "7ABz8qEFZTHPkovMDsmQkm64DZWN5wRtU7LEtD2ShkQ6": { name: "Red" },
+  "BXNiM7pqt9Ld3b2Hc8iT3mA5bSwoe9CRrtkSUs15SLWN": { name: "Absol" },
+  "5TuiERc4X7EgZTxNmj8PHgzUAfNHZRLYHKp4DuiWevXv": { name: "Rev" },
+  "4BdKaxN8G6ka4GYtQQWk4G4dZRUTX2vQH9GcXdBREFUk": { name: "Jijo" },
+  "831yhv67QpKqLBJjbmw2xoDUeeFHGUx8RnuRj9imeoEs": { name: "Trey" },
+  "DpNVrtA3ERfKzX4F8Pi2CVykdJJjoNxyY5QgoytAwD26": { name: "Gorilla Capital" },
+  "BCnqsPEtA1TkgednYEebRpkmwFRJDCjMQcKZMMtEdArc": { name: "Kreo" },
+  "7iabBMwmSvS4CFPcjW2XYZY53bUCHzXjCFEFhxeYP4CY": { name: "Leens" },
+  "8rvAsDKeAcEjEkiZMug9k8v1y8mW6gQQiMobd89Uy7qR": { name: "Casino" },
+  "D2wBctC1K2mEtA17i8ZfdEubkiksiAH2j8F7ri3ec71V": { name: "Dior" },
+  "DfMxre4cKmvogbLrPigxmibVTTQDuzjdXojWzjCXXhzj": { name: "Euris" },
+  "4DdrfiDHpmx55i4SPssxVzS9ZaKLb8qr45NKY9Er9nNh": { name: "Mr. Frog" },
+  "F2SuErm4MviWJ2HzKXk2nuzBC6xe883CFWUDCPz6cyWm": { name: "Earl" },
+  "CSHktdVEmJybwNR9ft3sDfSc2UKgTPZZ8km26XfHYZDt": { name: "Lynk" },
+  "7tiRXPM4wwBMRMYzmywRAE6jveS3gDbNyxgRrEoU6RLA": { name: "QtDegen" },
+  "CyaE1VxvBrahnPWkqm5VsdCvyS2QmNht2UFrKJHga54o": { name: "Cented" },
+  "34ZEH778zL8ctkLwxxERLX5ZnUu6MuFyX9CWrs8kucMw": { name: "Groovy" },
+  "2kv8X2a9bxnBM8NKLc6BBTX2z13GFNRL4oRotMUJRva9": { name: "Gh0stee" },
+  "5B52w1ZW9tuwUduueP5J7HXz5AcGfruGoX6YoAudvyxG": { name: "Yenni" },
+  "215nhcAHjQQGgwpQSJQ7zR26etbjjtVdW74NLzwEgQjP": { name: "OGAntD" },
+  "2YJbcB9G8wePrpVBcT31o8JEed6L3abgyCjt5qkJMymV": { name: "AI4N" },
+  "8deJ9xeUvXSJwicYptA9mHsU2rN2pDx37KWzkDkEXhU6": { name: "Cooker" },
+  "Gv7CnRo2L2SJ583XEfoKHKbmWK3wNoBDxVoJqMKJR4Nu": { name: "Robo" },
+  "41uh7g1DxYaYXdtjBiYCHcgBniV9Wx57b7HU7RXmx1Gg": { name: "Lowskii" },
+  "99i9uVA7Q56bY22ajKKUfTZTgTeP5yCtVGsrG9J4pDYQ": { name: "Zrool" },
+  "9yYya3F5EJoLnBNKW6z4bZvyQytMXzDcpU5D6yYr4jqL": { name: "Loopier" }
+};
